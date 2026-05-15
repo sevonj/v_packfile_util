d6423c288b412ebfb436cc3f9a66a74f913b33db
sevonj <100710152+sevonj@users.noreply.github.com>
2026-05-15 18:11:59 +0300

    chore: spdx lint

--- crates/v_modelview/build.rs
+++ crates/v_modelview/build.rs
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: sevonj
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const GENERATED_LICENSES_PLACEHOLDER: &str = "License information not generated.\nIf you plan to distribute this, you should look at tools/generate_licenses.py for license compliance.";
 
 fn main() {
